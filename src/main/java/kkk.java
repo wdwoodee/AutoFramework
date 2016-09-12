6aad7acf9db4027f9054167bd40ae01d53b8cdf7
wdwoodee <wdwoodee@163.com>
2016-09-12 20:05:13 +0800

    ggg

--- src/main/java/kkk.java
+++ src/main/java/kkk.java
@@ -2,11 +2,7 @@
 public class kkk {
 	public static void main(String[] args)
 	{
-		System.out.println("Welcomefdsfdsssssf!");
-		System.out.println("Welcome!");
-
-
-		System.out.println("sdgsdgs4!");
+		
 
     }
 } 
