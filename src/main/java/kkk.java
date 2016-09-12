f530e66f7df70bbc2b35a524215cacb8d262161b
luozt <18511246771@163.com>
2016-09-12 19:39:22 +0800

    delete

--- src/main/java/kkk.java
+++ src/main/java/kkk.java
@@ -2,9 +2,7 @@
 public class kkk {
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome!");
-		System.out.println("Welcome!");
-		System.out.println("Welcome!");
+		
 		
 	}
 } 
